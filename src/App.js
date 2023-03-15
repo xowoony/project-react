f92797f141eb7b4939bfe4428b28c2d28464c75b
xowoony <trumanblack@hanmail.net>
2023-03-15 17:31:04 +0900

    프로젝트 시작 전 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Button from "./Button"; // import 해주면 Button 을 아래에서 사용할 수 있는 상태가 된다.
+
 function App() {
   return (
     <div>
       <h1>Welcome back!!!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
