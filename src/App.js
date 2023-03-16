07fb37eed0e1b4618166bde838dea72c647785d4
xowoony <trumanblack@hanmail.net>
2023-03-16 19:15:43 +0900

    useEffect 사용하기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("call an api"); // 컴포넌트가 render 될 때 출력됨
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+
+  console.log("I run all the time!"); // 컴포넌트가 render 될 때 출력됨
+
+  // useEffect에게 우리의 function을 준다.(iRunOnlyOnce)
+  // 그리고 두 번째 argument로써 빈 array를 하나 준다.
+  useEffect(iRunOnlyOnce, []);
+
   return (
     <div>
       <h1>{counter}</h1>
