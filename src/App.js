94c17c7fe95a2f0e43f0fb00b5e726bd71392fea
xowoony <trumanblack@hanmail.net>
2023-03-16 21:11:20 +0900

    특정 부분이 변화할 때만 코드를 실행시키는 방법 학습

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  const iRunOnlyOnce = () => {
-    console.log("i run only once.");
-  };
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("저는 항상 실행됩니다!");
 
-  console.log("I run all the time!"); // 컴포넌트가 render 될 때 출력됨
+  // useEffect 사용하기 - API 처럼 딱 한번만 호출하고 다시는 호출하지 않을 경우 사용
+  useEffect(() => {
+    console.log("저는 한번만 실행됩니다.");
+  }, []); // 빈배열 - 리액트가 더이상 지켜볼 것이 없기 때문에 처음 한번만 실행됨.
 
-  // useEffect에게 우리의 function을 준다.(iRunOnlyOnce)
-  // 그리고 두 번째 argument로써 빈 array를 하나 준다.
-  useEffect(iRunOnlyOnce, []);
+  // 이 코드는 keyword가 변화할 때만 실행된다.
+  // if문을 사용하여 keyword가 뭐 하나라도 적힐때 && 5초과일 경우 console.log 되도록 조건을 준다.
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("찾는 영화는", keyword);
+    }
+  }, [keyword]); // 배열 안 keyword 를 넣어줌 - keyword가 바뀔때만 실행됨.
 
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="검색할 영화를 입력해주세요!"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
