8418e2f93a3d904485adf4964d24b3841364ba88
xowoony <trumanblack@hanmail.net>
2023-03-16 18:51:42 +0900

    랜더링 두번 되는 것 해결

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import Button from "./Button"; // import 해주면 Button 을 아래에서 사용할 수 있는 상태가 된다.
-import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api"); // 컴포넌트가 render 될 때 출력됨
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
