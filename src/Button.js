785767484d379a9ecda60adab487d147f7e287a1
xowoony <trumanblack@hanmail.net>
2023-03-16 14:22:08 +0900

    css 적용하는 법 학습

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types"; // import 해주면 밑에서 쓸 수 있다.
+
 function Button({text}) {
     return <button>{text}</button>;
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
 }
 
 export default Button;
