5ba8f9da7638fb79f38dcc7a32994c0cb844f880
xowoony <trumanblack@hanmail.net>
2023-03-17 12:56:33 +0900

    클릭시 요소 표시, 숨기기 구현

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  return <h1>안녕?</h1>;
+}
+
 function App() {
   const [showing, setShowing] = useState(false); // default 값은 false
-  const onClick = () => setShowing((prev) => !prev); // 반댓값을 리턴
+  const onClick = () => setShowing((prev) => !prev); // 클릭시 반댓값을 리턴
 
   return (
     <div>
+      {/* js를 쓸때는 중괄호! */}
+      {/* showing이 참일 때 <Hello />를 반환, 아닐 때는 null */}
+      {showing ? <Hello /> : null}
+      {/* 버튼 - showing이 참일때 Hide, 아닐때 Show 로 텍스트가 변환 */}
       <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
