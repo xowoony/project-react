3cfc66dfbc05d444e6c01ca08ae8d6e2739f2adc
xowoony <trumanblack@hanmail.net>
2023-03-17 12:04:11 +0900

    클릭시 반댓값을 리턴

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("저는 한번만 실행됩니다.");
-  }, []);
-  
-  useEffect(() => {
-      console.log("저는 키워드가 변화할 때만 실행됩니다.");
-  }, [keyword]); 
-
-  useEffect(() => {
-    console.log("저는 카운터가 변화할 때만 실행됩니다.");
-}, [counter]); 
-
-
-useEffect(() => {
-  console.log("저는 카운터 또는 키워드가 변화할 때만 실행됩니다.");
-}, [counter, keyword]); 
+  const [showing, setShowing] = useState(false); // default 값은 false
+  const onClick = () => setShowing((prev) => !prev); // 반댓값을 리턴
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="검색할 영화를 입력해주세요!"
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
