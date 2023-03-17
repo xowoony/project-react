c00ee3840fc2364c4ee4becc37f3d35028963305
xowoony <trumanblack@hanmail.net>
2023-03-17 17:23:14 +0900

    toDo 첫번째 파트

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { useState, useEffect } from "react";
-
-// useEffect는 컴포넌트가 처음 생성될 때 실행된다
-// 이것으로 인해 컴포넌트가 언제 생성되었는지, 언제 사라졌는지 알 수 있게 됨.
-// 컴포넌트가 파괴될 대는 react.js가 hiFn이 리턴한 function 을 실행하게 된다.
-function Hello() {
-  useEffect(()=>{
-    console.log("Hi");
-    return () => console.log("bye"); // cleanup function
-  }, []);
-  return <h1>안녕?</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false); // default 값은 false
-  const onClick = () => setShowing((prev) => !prev); // 클릭시 반댓값을 리턴
+  const [toDo, setToDo] = useState("");
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return; // 만약 toDo 값이 비었다면 이 함수를 작동하지 않게함
+    }
+    setToDo(""); // toDo 값이 있다면 toDo 즉 input 값을 ""로 비워줌
+    // toDo 값은 input과 연결되어 있음 / input을 비워준다.
+  };
 
   return (
     <div>
-      {/* js를 쓸때는 중괄호! */}
-      {/* showing이 참일 때 <Hello />를 반환, 아닐 때는 null */}
-      {showing ? <Hello /> : null}
-      {/* 버튼 - showing이 참일때 Hide, 아닐때 Show 로 텍스트가 변환 */}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write your to do..."
+        />
+        <button>처음으로</button>
+      </form>
+      
     </div>
   );
 }
