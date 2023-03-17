901eaafb181d42c3b3e9042d75b3152eb6f887e4
xowoony <trumanblack@hanmail.net>
2023-03-17 14:15:09 +0900

    cleanup function 연습

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import { useState, useEffect } from "react";
 // 이것으로 인해 컴포넌트가 언제 생성되었는지, 언제 사라졌는지 알 수 있게 됨.
 // 컴포넌트가 파괴될 대는 react.js가 hiFn이 리턴한 function 을 실행하게 된다.
 function Hello() {
-
-  function byFn(){
-    console.log("컴포넌트가 사라졌어요");
-  }
-
-
-  function hiFn() {
-    console.log("컴포넌트가 만들어졌어요!");
-    return byFn; // cleanup function
-  }
-
-  useEffect(hiFn, []);
-
+  useEffect(()=>{
+    console.log("Hi");
+    return () => console.log("bye"); // cleanup function
+  }, []);
   return <h1>안녕?</h1>;
-
 }
 
 function App() {
