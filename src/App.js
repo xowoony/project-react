ca71c0cbce2415576258c49c61029d5b0232fe88
xowoony <trumanblack@hanmail.net>
2023-03-18 19:17:56 +0900

    로딩시 select 없애기 추가함

--- src/App.js
+++ src/App.js
@@ -14,23 +14,32 @@ function App() {
   }, []); // [] 적어주어 한번만 로딩되고 끝
 
   return (
-    <div style={{fontFamily: "'Gowun Dodum', sans-serif",
-    color:"rgb(255,255,255)", backgroundColor:"rgb(0,0,0)" }}>
-      <h1 style={{margin:"0"}}>The Coins! (총 {coins.length}개)</h1>
-      {loading ? <strong>loading...</strong> : null}{" "}
-      {/*로딩 중일때 loading...가 표시됨 아닐때는 문구가 사라짐*/}
-      <ul>
-        {/*  index가 필요없는 이유 : ID를 갖고 있기 때문에 key를 ID로 써도 되기 때문
+    <div
+      style={{
+        fontFamily: "'Gowun Dodum', sans-serif",
+        color: "rgb(255,255,255)",
+        backgroundColor: "rgb(0,0,0)",
+      }}
+    >
+      <h1 style={{ margin: "0" }}>The Coins! (총 {coins.length}개)</h1>
+      {loading ? (
+        <strong>loading...</strong>
+      ) : (
+        <select>
+          {/*  index가 필요없는 이유 : ID를 갖고 있기 때문에 key를 ID로 써도 되기 때문
            첫번째 리스트로 name을 보여주고 싶기 때문에 coins.name을 작성해준다.
            coin은 coins array안에 있는 각각의 coin을 의미한다. */}
-        {coins.map((coin) => (
-          <li>
-            {coin.rank} {/*코인 랭킹*/}
-            {"위 : "}
-            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD  {/*이름 (심볼) : 가격 USD*/}
-          </li>
-        ))}
-      </ul>
+          {coins.map((coin) => (
+            <option>
+              {coin.rank} {/*코인 랭킹*/}
+              {"위 : "}
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD{" "}
+              {/*이름 (심볼) : 가격 USD*/}
+            </option>
+          ))}
+        </select>
+      )}
+      {/*로딩 중일때 loading...가 표시됨 아닐때는 문구가 사라짐*/}
     </div>
   );
 }
