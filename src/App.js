6e7dd17bbdce83353ddaeffaad61f611d45a58e7
xowoony <trumanblack@hanmail.net>
2023-03-18 16:36:28 +0900

    코인 총 갯수 불러오기 추가

--- src/App.js
+++ src/App.js
@@ -8,19 +8,27 @@ function App() {
     fetch("https://api.coinpaprika.com/v1/tickers") // URL fetch
       .then((response) => response.json()) //URL fetch 한걸 응답을 받아 json으로 리턴
       .then((json) => {
-        setCoins(json);// 리턴한 json값을 setCoins 해줌
+        setCoins(json); // 리턴한 json값을 setCoins 해줌
         setLoading(false); // setCoins 해주었기 때문에 (다 불러왔기 때문에) 로딩을 false로 바꾸어주기
-      }); 
+      });
   }, []); // [] 적어주어 한번만 로딩되고 끝
 
   return (
-    <div style={{fontFamily : "'Gowun Dodum', sans-serif"}}>
-      <h1>The Coins!</h1>
-      {loading ? <strong>loading...</strong> : null} {/*로딩 중일때 loading...가 표시됨 아닐때는 문구가 사라짐*/}
+    <div style={{ fontFamily: "'Gowun Dodum', sans-serif" }}>
+      <h1>The Coins! (총 {coins.length}개)</h1>
+      {loading ? <strong>loading...</strong> : null}{" "}
+      {/*로딩 중일때 loading...가 표시됨 아닐때는 문구가 사라짐*/}
       <ul>
-          {coins.map( (coins) => <li>{coins.rank}{"위 : "}{coins.name}</li> )}
-          {/*  index가 필요없는 이유 : ID를 갖고 있기 때문에 key를 ID로 써도 되기 때문
-           첫번째 리스트로 name을 보여주고 싶기 때문에 coins.name을 작성해준다. */}
+        {/*  index가 필요없는 이유 : ID를 갖고 있기 때문에 key를 ID로 써도 되기 때문
+           첫번째 리스트로 name을 보여주고 싶기 때문에 coins.name을 작성해준다.
+           coin은 coins array안에 있는 각각의 coin을 의미한다. */}
+        {coins.map((coin) => (
+          <li>
+            {coin.rank} {/*코인 랭킹*/}
+            {"위 : "}
+            {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD  {/*이름 (심볼) : 가격 USD*/}
+          </li>
+        ))}
       </ul>
     </div>
   );
