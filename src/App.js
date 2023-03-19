1f570f7fc01552337fd732ea3714e5adf9549225
xowoony <trumanblack@hanmail.net>
2023-03-19 21:23:24 +0900

    영화 타이틀 불러오기 완료, 글씨체 변경

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
-  const [loading, setLoading] = useState(true); // 기본값 true
-  const[movies, getMovies] = useState([]); // 기본값으로 빈배열을 둔다.
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
 
-  // 한번만 실행됨
+  // 새로 생성함
+  // 비동기(Asynchronous : 동시에 일어나지 않는)
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  // useEffect를 이용하여 getMovies를 호출할 것이다.
   useEffect(() => {
-    fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    )
-      .then((response) => response.json())
-      .then((json) => getMovies(json.data.movies));
-      setLoading(false)
+    getMovies();
   }, []);
-  console.log(movies);
-  return <div>{loading ? <h3>로딩 중...</h3> : null}</div>;
+
+  return (
+    <div>
+      {loading ? (
+        <h3>로딩 중...</h3>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+            </div> // key는 unique 여야 한다. fetch 사이트에서 보면 영화마다 고유한 id가 있기 때문에 그걸 가져다 쓸거임.
+          ))}
+        </div>
+      )}
+    </div>
+  );
+
+ 
 }
 
 export default App;
