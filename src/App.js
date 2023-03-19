e9afbed965c8e636f9ae43ecd46b42ba5e779c81
xowoony <trumanblack@hanmail.net>
2023-03-19 18:39:41 +0900

    영화정보 api fetch하기

--- src/App.js
+++ src/App.js
@@ -1,81 +1,19 @@
 import { useEffect, useState } from "react";
-import styles from "./App.module.css";
-import { AiFillCaretRight } from "react-icons/ai";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [pay, setPay] = useState(1);
+  const [loading, setLoading] = useState(true); // 기본값 true
 
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setPay(1);
-  };
-
-  const handleInput = (event) => {
-    setPay(event.target.value);
-  };
-
+  // 한번만 실행됨
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
+    fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    )
       .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
+      .then((json) => console.log(json));
   }, []);
 
-  return (
-    <div className={styles.center}>
-      <h1 className={styles.title}>코인 변환기</h1>
-      <div className={styles.sub}>
-        {loading ? "" : ` (총 ${coins.length} 가지 코인 선택 가능)`}
-      </div>
-      {loading ? (
-        <strong>로딩중입니다.. 잠시만 기다려주세요!</strong>
-      ) : (
-        <select onChange={onChange} className={styles.select}>
-          <option>코인 종류를 선택해주세요!</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <table className={styles.table}>
-        <tbody>
-          <tr>
-            <td>
-              <div>구매에 사용할 금액을 입력하세요</div>
-              <input
-                className={styles.input}
-                type="number"
-                value={pay}
-                onChange={handleInput}
-                placeholder="달러 ($)"
-              />{" "}
-              $
-            </td>
-            <td colSpan={2}>
-              <div className={styles.width}>
-                <AiFillCaretRight />
-              </div>
-            </td>
-            <td>
-              <div>구매할 수 있는 코인은 다음과 같습니다.</div>
-              <div className={styles.center}>{pay / cost}</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+
+  return <div>{loading ? <h3>로딩 중...</h3> : null}</div>;
 }
+
 export default App;
