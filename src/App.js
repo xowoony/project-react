a863367c06de0a9f47b0a07ac582984ea2548a4b
xowoony <trumanblack@hanmail.net>
2023-03-19 18:45:02 +0900

    movie 정보 콘솔에 찍어보기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true); // 기본값 true
+  const[movies, getMovies] = useState([]); // 기본값으로 빈배열을 둔다.
 
   // 한번만 실행됨
   useEffect(() => {
@@ -9,10 +10,10 @@ function App() {
       `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
     )
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => getMovies(json.data.movies));
+      setLoading(false)
   }, []);
-
-
+  console.log(movies);
   return <div>{loading ? <h3>로딩 중...</h3> : null}</div>;
 }
 
