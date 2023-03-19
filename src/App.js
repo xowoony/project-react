b95c83f0ad97e0caa4b2d4f8d9126f4a98d4cd50
xowoony <trumanblack@hanmail.net>
2023-03-19 14:23:01 +0900

    코인 변환기 만드는 중

--- src/App.js
+++ src/App.js
@@ -1,49 +1,75 @@
 import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]); // default는 빈배열, data를 coins로 둠
+  const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [pay, setPay] = useState(1);
+
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setPay(1);
+  };
+
+  const handleInput = (event) => {
+    setPay(event.target.value);
+  };
+
   useEffect(() => {
-    // fetch를 한다. then을 써서 응답을 받아서 response.json을 리턴해준다.
-    fetch("https://api.coinpaprika.com/v1/tickers") // URL fetch
-      .then((response) => response.json()) //URL fetch 한걸 응답을 받아 json으로 리턴
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
       .then((json) => {
-        setCoins(json); // 리턴한 json값을 setCoins 해줌
-        setLoading(false); // setCoins 해주었기 때문에 (다 불러왔기 때문에) 로딩을 false로 바꾸어주기
+        setCoins(json);
+        setLoading(false);
       });
-  }, []); // [] 적어주어 한번만 로딩되고 끝
+  }, []);
 
   return (
-    <div
-      style={{
-        fontFamily: "'Gowun Dodum', sans-serif",
-        color: "rgb(255,255,255)",
-        backgroundColor: "rgb(0,0,0)",
-      }}
-    >
-      <h1 style={{ margin: "0" }}>
-        The Coins! {loading ? "" : `(총 ${coins.length}개)`}
-      </h1>
+    <div className={styles.center}>
+      <h1 className={styles.title}>코인 변환기</h1>
+      <div className={styles.sub}>
+        {loading ? "" : ` (총 ${coins.length} 가지 코인 선택 가능)`}
+      </div>
       {loading ? (
-        <strong>loading...</strong>
+        <strong>로딩중입니다.. 잠시만 기다려주세요!</strong>
       ) : (
-        <select>
-          {/*  index가 필요없는 이유 : ID를 갖고 있기 때문에 key를 ID로 써도 되기 때문
-           첫번째 리스트로 name을 보여주고 싶기 때문에 coins.name을 작성해준다.
-           coin은 coins array안에 있는 각각의 coin을 의미한다. */}
-          {coins.map((coin) => (
-            <option>
-              {coin.rank} {/*코인 랭킹*/}
-              {"위 : "}
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD{" "}
-              {/*이름 (심볼) : 가격 USD*/}
+        <select onChange={onChange} className={styles.select}>
+          <option>코인 종류를 선택해주세요!</option>
+          {coins.map((coin, index) => (
+            <option
+              key={index}
+              value={coin.quotes.USD.price}
+              id={coin.symbol}
+              symbol={coin.symbol}
+            >
+              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
-      {/*로딩 중일때 loading...가 표시됨 아닐때는 문구가 사라짐*/}
+      <table>
+        <tbody>
+          <tr>
+              <td>
+                <div>구매에 사용할 금액을 입력하세요</div>
+                <input
+                  type="number"
+                  value={pay}
+                  onChange={handleInput}
+                  placeholder="dollor"
+                />
+              </td>
+              <td colSpan={2}> ======> </td>
+              <td>
+                <div>구매할 수 있는 코인은 다음과 같습니다</div>
+                <div>{pay / cost}</div>
+              </td>
+          </tr>
+  
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,13 @@
-// import PropTypes from "prop-types"; // import 해주면 밑에서 쓸 수 있다.
-// import styles from "./Button.module.css";
+import PropTypes from "prop-types"; // import 해주면 밑에서 쓸 수 있다.
+import styles from "./Button.module.css";
 
-// function Button({ text }) {
-//   return <button className={styles.title}>{text}</button>; // className 부여 styles.btn 
+function Button({ text }) {
+  return <button className={styles.title}>{text}</button>; // className 부여 styles.btn 
 
-// }
+}
 
-// Button.propTypes = {
-//   text: PropTypes.string.isRequired,
-// };
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
-// export default Button;
+export default Button;
