d305d68f4cbc62307d82217e38f6c0be382ee485
xowoony <trumanblack@hanmail.net>
2023-03-20 15:37:33 +0900

    Movie관련 코드를 Movie.js에 옮기고 App.js에서 <Movie /> 사용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import styles from "./App.module.css";
+import Movie from "./Movie";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
-  // 새로 생성함
-  // 비동기(Asynchronous : 동시에 일어나지 않는)
   const getMovies = async () => {
     const json = await (
       await fetch(
@@ -18,7 +17,7 @@ function App() {
     setLoading(false);
   };
 
-  // useEffect를 이용하여 getMovies를 호출할 것이다.
+  // useEffect를 이용하여 getMovies를 호출
   useEffect(() => {
     getMovies();
   }, []);
@@ -30,18 +29,15 @@ function App() {
       ) : (
         <div>
           {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image}></img>
-              {/* 이 컴포넌트들은 movies 배열에 있는 각 movie에서 변형되어 나온 것임. */}
-              <h2>{movie.title}</h2> {/*영화제목 불러오기*/}
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li> // map을 쓸 때에는 key를 element로 줘야함. 주지 않으면 에러
-                  // 장르는 ID등 어떠한 고유값이 없음. 따라서 key를 g로 줬는데, g가 고유한 값인 한 상관 없음.
-                ))}
-              </ul>
-              <p>{movie.summary}</p> {/*영화 줄거리 불러오기*/}
-            </div> // key는 unique 여야 한다. fetch 사이트에서 보면 영화마다 고유한 id가 있기 때문에 그걸 가져다 쓸거임.
+            <Movie
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+            // 원래 medium_cover_image 이지만 mediumCoverImage 로 이름을 바꾸거나 완전 다르게 바꿔 주어도 됨.
+            // 난 여기서 coverImg라고 사용하겠음.
+            // 여기서 바꿔줬으면 Movies.js에서도 바꾸어주어야 함.
           ))}
         </div>
       )}
---
+++ src/Movie.js
@@ -0,0 +1,22 @@
+// movie component를 이곳에 담을 것이다
+// Movie component가 {medium_cover_image,title,summary,genres} 이 정보들을 parent component로 부터 받아온다
+// 이 Movie.js를 App.js에서 <Movie /> 하여 가져다 쓸 수 있다. 맨 위에 import Movie from "./Movie" 적어주어야 하고
+// Movie({medium_cover_image, title, summary, genres}) {} 안의 것들은 이름을 바꾸어도 상관 없다. 그래서 너무 긴 이름을 짧게 coverImg로 바꿔주었다.
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <div>
+        <img src={coverImg} />
+        <h2>{title}</h2>
+        <ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+        <p>{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Movie;
