c7bf96024f99ddfa2ee85f641dd5993f1d0f8ac0
xowoony <trumanblack@hanmail.net>
2023-03-25 11:28:04 +0900

    Updates

---
+++ precache-manifest.c1886c2aa200f6c089095e203929dbce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e4fe839d23987f26f9a",
+    "url": "/project-react/static/css/main.6f918124.chunk.css"
+  },
+  {
+    "revision": "7e4fe839d23987f26f9a",
+    "url": "/project-react/static/js/main.7e4fe839.chunk.js"
+  },
+  {
+    "revision": "a7d099846b7260d58b10",
+    "url": "/project-react/static/js/1.a7d09984.chunk.js"
+  },
+  {
+    "revision": "6117a6ef658c1bfb35f9",
+    "url": "/project-react/static/js/runtime~main.6117a6ef.js"
+  },
+  {
+    "revision": "585560260cafd62747ab27e048a68ef2",
+    "url": "/project-react/index.html"
+  }
+];
