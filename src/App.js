4f5dad3d1a5c44d458f284de6bd37c836312f7a6
xowoony <trumanblack@hanmail.net>
2023-03-25 11:09:40 +0900

    package.json 버전 관련 문제 해결

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import  React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import MovieDetail from "../components/MovieDetail";
 import "../css/MovieDetail.css"
